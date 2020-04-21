d75a01629a8cfc626219f8a9807ec0b5f889406e
Max Blancarte <blancarte@gmail.com>
2020-04-21 00:18:05 -0700

    redone interface added date support to database

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//jshint esversion:6
+
 // Const variables
 
 const trigl = 801.2;
@@ -18,6 +18,7 @@ const z = 11.42
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const moment = require("moment");
 
 // Start up
 
@@ -31,39 +32,60 @@ mongoose.connect('mongodb://localhost:27017/QuimicaDB', {useNewUrlParser: true, 
 
 const pacientesSchema = {
   Nombre: String,
+  ApeidoPaciente: String,
   NombreDoctor: String,
   Folio: String,
   FechaNacimiento: Number,
   TomaDeMuestra: Number,
   FechaReporte: Number,
-  Fecha: Number,
+  Fecha: Date
 };
 
   const Pacientes = mongoose.model("Pacientes", pacientesSchema);
 
 app.get("/", function (req, res) {
-  console.log("entered app");
-  res.redirect("/index.html");
+  res.send("On the root");
 });
 
 
-app.post("/", function (req, res){
+// app.post("/", function (req, res) {
+//   res.send("sent info to root");
+//   console.log("entered root");
+
+// });
+
+app.get ("/registro", function (req, res) {
+  res.sendFile(__dirname + "/public/Registro.html");
+});
+
+
+app.post("/", function (req, res) {
 
   const NombrePaciente = req.body.nombrePaciente;
-  const NombreDoctor = req.body.nombreDoctor;
-  const Folio = req.body.folio;
+  const pacienteApeido = req.body.pacienteApeido;
+  const doctor = req.body.doctor;
+  const folio = req.body.folio;
+  const fechaNacimiento = moment(req.body.fechaNacimiento, 'YYYY-MM-DD', 'us', true);
 
-  const PacientesL = new Pacientes({
-    Nombre: NombrePaciente,
-    NombreDoctor: NombreDoctor,
-    Folio: Folio,
-  });
 
-  //save to database
-    PacientesL.save();
+  console.log("nombre del paciente: " + NombrePaciente);
+  console.log("apeido del paciente: " + pacienteApeido);
+  console.log("Doctor " + doctor);
+  console.log("folio" + folio);
+  console.log("nacimiento " + fechaNacimiento);
+
+  // const PacientesL = new Pacientes({
+  //   Nombre: NombrePaciente,
+  //   ApeidoPaciente: pacienteApeido,
+  //   NombreDoctor: doctor,
+  //   Folio: folio,
+  //   FechaNacimiento: fechaNacimiento
+  // });
+
+  // //save to database
+  //   PacientesL.save();
+    res.send("data submitted");
 });
-
-
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
